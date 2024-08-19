0b87832a8fc0fe0587d5edf54c7cea11facb428c
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-08-19 13:19:53 -0300

    REFACTOR: structure folders

---
+++ lib/src/modules/auth/external/datasources/server_routes.dart
@@ -0,0 +1,5 @@
+const serverAdrees = 'http://127.0.0.1:10100';
+
+const signOUUserRoute = '$serverAdrees/sign_uo-user';
+const loginRoute = '$serverAdrees/login';
+
--- lib/src/modules/task/external/datasources/server_routes.dart
+++ lib/src/modules/task/external/datasources/server_routes.dart
@@ -1,7 +1,5 @@
 const serverAdrees = 'http://127.0.0.1:10100';
 
-const signOUUserRoute = '$serverAdrees/sign_uo-user';
-const loginRoute = '$serverAdrees/login';
 const addTaskRoute = '$serverAdrees/add_task';
 
 //Ouve -> Responde
