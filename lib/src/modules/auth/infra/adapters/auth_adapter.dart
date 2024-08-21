28f9102b1c2acb38b2c7d30d7b16f38c16bfebda
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-08-21 19:41:34 -0300

    FEAT:  auth adapter - infra

---
+++ lib/src/modules/auth/infra/adapters/auth_adapter.dart
@@ -0,0 +1,31 @@
+import 'dart:typed_data';
+import 'package:project_list_fliutter/src/modules/auth/infra/comm_packages/proto/user.pb.dart';
+
+class AuthAdapter {
+
+  static User decodeProto(Uint8List encodedUserProto) {
+    try {
+      final userProto = User.fromBuffer(encodedUserProto); 
+      return User(
+        id: userProto.id,
+        name: userProto.name,
+        password: userProto.password,
+      );
+    } catch (e) {
+      throw Error;
+    }
+  }
+
+  static Uint8List encodeProto(User userModel) {
+    try {
+      final userProto = User()
+        ..id = userModel.id
+        ..name = userModel.name
+        ..password = userModel.password;
+
+      return userProto.writeToBuffer();
+    } catch (e) {
+      throw Error;
+    }
+  }
+}
