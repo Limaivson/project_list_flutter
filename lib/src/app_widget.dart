191e4b408da16225a609d09686be10d7cff097c5
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-08-21 14:13:17 -0300

    FEAT: create a model auth page and google fonts dependences

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -11,15 +11,9 @@ class AppWidget extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Lista',
-      theme: ThemeData(
-        colorScheme: const ColorScheme.light(
-          background: Color(0xFFFFFFFF),
-          // onBackground: Color(0xFFF8FDFF),
-          // primary: Color(0xFF144232),
-          // secondary: Color(0xFFBCEBDE),
-          // onSecondary: Colors.black,
-          // onPrimary: Colors.white,
-        ),
+       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFEFEFEF), 
+        primarySwatch: Colors.blue,
       ),
        routerConfig: Modular.routerConfig,
     ); //added by extension
--- lib/src/modules/auth/presenter/pages/auth.dart
+++ lib/src/modules/auth/presenter/pages/auth.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:window_manager/window_manager.dart';
@@ -11,19 +13,41 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> with WindowListener {
-@override
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Form(child: 
-        Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Email'
+    return Scaffold(
+      body: Container(
+
+        child: const Form(
+          child: Column(
+            children: [
+              Text('Welcome'),
+              Text('Enter your credentials'),
+              Column(
+                children: [
+                  TextField(
+                decoration: InputDecoration(
+                hintText: 'Enter your email',
+                ),
               ),
-            )
-          ],
-        )
+              TextField(
+                decoration: InputDecoration(
+                hintText: 'Enter your password',
+                ),
+              ),
+              TextButton(onPressed: null, child: Text('Login')),
+                ],
+              ),
+              InkWell(child: Text('Forget password?')),
+              Row(
+                children: [
+                  Text('Dont have an account?'),
+                  InkWell(child: Text('SignUp')),
+                ],
+              )
+            ],
+          )
+        ),
       ),
     );
   }
