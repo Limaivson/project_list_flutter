aa09d785aa0491c6a06cd07ed1e9a7778e10ebc8
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-08-23 16:40:36 -0300

    FEAT: implements infra datasource and respository task module

--- lib/src/modules/task/infra/datasources/task_datasource.dart
+++ lib/src/modules/task/infra/datasources/task_datasource.dart
@@ -1,40 +1,7 @@
-import 'package:project_list_fliutter/src/modules/task/domain/repositories/tasks_repository.dart';
-import 'package:project_list_fliutter/src/modules/task/infra/adapters/task_adapter.dart';
-import 'package:project_list_fliutter/src/modules/task/infra/datasources/task_datasource.dart';
+import 'dart:typed_data';
 
 abstract class TaskDatasource {
-  
-}
-
-class TaskRepositoryImpl implements TaskRepository {
-  final TaskDatasource datasource;
-
-  TaskRepositoryImpl(this.datasource);
-  
-  @override
-  Future<void> addTask(String task) {
-    // TODO: implement addTask
-    throw UnimplementedError();
-  }
-  
-  @override
-  Future<void> deleteTask(int taskId) {
-    // TODO: implement deleteTask
-    throw UnimplementedError();
-  }
-  
-  @override
-  Future<List<String>> getTasks() {
-    // TODO: implement getTasks
-    throw UnimplementedError();
-  }
-  
-  @override
-  Future<void> updateTask(String task) {
-    // TODO: implement updateTask
-    throw UnimplementedError();
-  }
-
-  
- 
+  Future<List<Uint8List>> getAllTasks();
+  Future<void> saveTask(Uint8List task);
+  Future<void> deleteTask(String taskId);
 }
