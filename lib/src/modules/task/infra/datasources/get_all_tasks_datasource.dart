e0c4d874846b28741df97bcfa72c195668f87655
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-08-27 16:49:25 -0300

    FEAT: create task repository in Infra - module Task

---
+++ lib/src/modules/task/infra/datasources/get_all_tasks_datasource.dart
@@ -0,0 +1,5 @@
+import 'dart:typed_data';
+
+abstract class IGetAllTasksDatasource {
+  Future<List<Uint8List>> getAllTasks();
+}
---
+++ lib/src/modules/task/infra/repositories/get_task_repository.dart
@@ -0,0 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:project_list_fliutter/src/modules/task/domain/repositories/get_task_repository.dart';
+import 'package:project_list_fliutter/src/modules/task/infra/adapters/task_adapter.dart';
+import 'package:project_list_fliutter/src/modules/auth/infra/datasources/get_user_datasource.dart';
+import 'package:project_list_fliutter/src/modules/task/infra/datasources/get_all_tasks_datasource.dart';
+
+// class IGetTaskRepositoryImpl implements IGetAllTasksDatasource {
+
+// }
