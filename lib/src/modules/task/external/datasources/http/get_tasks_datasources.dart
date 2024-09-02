97b8b4aa2616b41317f58feb74697e5200765891
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-09-02 16:20:37 -0300

    REFACTOR: clean code architecture

--- lib/src/modules/task/external/datasources/http/get_tasks_datasources.dart
+++ lib/src/modules/task/external/datasources/http/get_tasks_datasources.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:project_list_fliutter/src/modules/task/external/datasources/server_routes.dart';
 import 'package:project_list_fliutter/src/modules/task/infra/comm_packages/proto/pb/tasks.pb.dart';
 import 'package:project_list_fliutter/src/modules/task/infra/datasources/get_all_tasks_datasource.dart';
+import 'package:project_list_fliutter/src/modules/task/infra/adapters/task_adapter.dart';
 
 class GetTaskDatasourceExternal implements IGetAllTasksDatasource {
   final http.Client client;
@@ -20,16 +20,12 @@ class GetTaskDatasourceExternal implements IGetAllTasksDatasource {
         headers: {'id': userId},
       );
 
-      //refaz - proto diretamente
       if (response.statusCode == 200) {
         final Uint8List responseBodyBytes = response.bodyBytes;
         final tasksProto = Tasks.fromBuffer(responseBodyBytes);
+      
         return tasksProto.tasks
-            .map((task) => Task(
-                  id: task.id,
-                  task: task.task,
-                  userId: task.userId,
-                ))
+            .map((taskProto) => TaskAdapter.decodeProto(taskProto.writeToBuffer()))
             .toList();
       } else {
         throw Exception('Failed to load tasks');
